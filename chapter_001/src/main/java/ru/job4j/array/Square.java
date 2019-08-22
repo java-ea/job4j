c3089635dfa40d2ea634b3f1152eaf5688f6152c
Pavlo Stefiniv <stefiniv4w@gmail.com>
2019-08-23 01:52:06 +0300

    Заполнить массив степенями чисел.

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+/**
+ * Square.
+ *
+ * @author Pavlo Stefiniv (stefiniv4w@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    /**
+     * calculate.
+     *
+     * @param bound - Размер массива.
+     * @return - массив чисел от 1 до bound, возведенными в квадрат.
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        int q=1;
+        for (int i = 0; i < bound ; i++) {
+            rst[i]=q*q;
+            q++;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+/**
+ * SquareTest.
+ *
+ * @author Pavlo Stefiniv (stefiniv4w@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void whenBound0Then() {
+        int bound = 0;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{};
+        assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void whenBound5Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+
+}
