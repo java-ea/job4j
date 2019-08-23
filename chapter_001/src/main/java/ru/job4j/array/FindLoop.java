b8cdabf427821c32e7c6cf220b424b5a60a56eca
Pavlo Stefiniv <stefiniv4w@gmail.com>
2019-08-23 17:02:21 +0300

    Сортировка выборкой.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -48,4 +48,26 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /**
+     * sort.
+     * Сортировка выборкой.
+     *
+     * @param data - входящий массив.
+     * @return data - отсортированный массив.
+     */
+    public int[] sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
+                }
+            }
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+        return data;
+    }
 }
