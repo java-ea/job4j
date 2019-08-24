3910413ac30c7f853ba0a4909bd24525a6b6d4d1
Pavlo Stefiniv <stefiniv4w@gmail.com>
2019-08-24 21:06:18 +0300

    Массив заполнен true или false. 0.2

--- chapter_001/src/test/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 /**
  * CheckTest.
  *
  * @author Pavlo Stefiniv (stefiniv4w@gmail.com)
- * @version 0.1
- * @since 23.08.2019
+ * @version 0.2
+ * @since 24.08.2019
  */
 public class CheckTest {
     @Test
@@ -19,32 +20,19 @@ public class CheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(true));
     }
+
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
         Check check = new Check();
-        boolean[] input = new boolean[] {true, false, true};
-        boolean result = check.mono(input);
-        assertThat(result, is(false));
-    }
-    @Test
-    public void when3True1FalseThenFalse() {
-        Check check = new Check();
-        boolean[] input = new boolean[]{true, true, false, true};
-        boolean result = check.mono(input);
-        assertThat(result, is(false));
-    }
-    @Test
-    public void when4True1FalseThenFalse() {
-        Check check = new Check();
-        boolean[] input = new boolean[]{false, true, true, true,true};
+        boolean[] input = new boolean[]{true, false, true};
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
 
     @Test
-    public void when3True2FalseThenFalse() {
+    public void when3True1FalseThenFalse() {
         Check check = new Check();
-        boolean[] input = new boolean[]{true, false, true, false, true};
+        boolean[] input = new boolean[]{true, true, true, false};
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
