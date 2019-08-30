e5872b4db651086cce9b1d550c1668e6610b93c6
Pavlo Stefiniv <stefiniv4w@gmail.com>
2019-08-30 16:38:39 +0300

    Котики обормотики.

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+/**
+ * Cat - Котики обормотики.
+ *
+ * @author Pavlo Stefiniv (stefiniv4w@gmail.com)
+ * @version 0.1
+ * @since 30.08.2019.
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * Dog.
+ *
+ * @author Pavlo Stefiniv (stefiniv4w@gmail.com)
+ * @version 0.1
+ * @since 30.08.2019.
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
