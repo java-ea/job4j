4918f68f5e45b0a49c1ecfb0e879c5af7d08cd84
Pavlo Stefiniv <stefiniv4w@gmail.com>
2019-12-16 17:12:18 +0200

    validate fixes.

--- chapter_002/src/main/java/ru/job4j/oopexams/Machine.java
+++ chapter_002/src/main/java/ru/job4j/oopexams/Machine.java
@@ -1,19 +1,19 @@
-package ru.job4j.oopExams;
+package ru.job4j.oopexams;
 
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int count = 0;
         int change = money - price;
-        for (int i = 0; i < COINS.length; i++) {
-            while (change - COINS[i] >= 0) {
-                rsl[count] = COINS[i];
-                change -= COINS[i];
+        for (int i = 0; i < coins.length; i++) {
+            while (change - coins[i] >= 0) {
+                rsl[count] = coins[i];
+                change -= coins[i];
                 count++;
                 size++;
             }
--- chapter_002/src/test/java/ru/job4j/oopexams/MachineTest.java
+++ chapter_002/src/test/java/ru/job4j/oopexams/MachineTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.oopExams;
+package ru.job4j.oopexams;
 
 import org.junit.Test;
 
